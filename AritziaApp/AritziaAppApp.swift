79f981f77996f7f421ac8a91141d6eba062edfd2
Shelley Chen <shelleychen318@gmail.com>
2023-02-18 18:44:05 -0500

    Initial Commit

---
+++ AritziaApp/AritziaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AritziaAppApp.swift
+//  AritziaApp
+//
+//  Created by Shelley Chen on 2023-02-18.
+//
+
+import SwiftUI
+
+@main
+struct AritziaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AritziaApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AritziaApp
+//
+//  Created by Shelley Chen on 2023-02-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
