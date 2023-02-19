a454dfc3ea8c2c5df395a6c4f9538175f1a01000
Shelley Chen <shelleychen318@gmail.com>
2023-02-18 20:30:44 -0500

    displayed title as navigationBarTitle and added home image

--- AritziaApp/ContentView.swift
+++ AritziaApp/ContentView.swift
@@ -8,22 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    init() {
+        UINavigationBar.appearance().titleTextAttributes = [.font : UIFont(name: "Georgia-Bold", size: 20)!]
+    }
+    
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading, spacing: 24) {
-                    
+                    Image("Image")
                 }
-                .padding()
                 .frame(maxWidth: .infinity)
             }
+            .navigationTitle("ARITZIA")
+            .navigationBarTitleDisplayMode(.inline)
             .toolbar {
-                ToolbarItem(placement: .principal) {
-                    // MARK: Title
-                    Text("ARITZIA")
-                        .font(.title2)
-                        .bold()
-                }
+//                ToolbarItem(placement: .principal) {
+//                    // MARK: Title
+//                    Text("ARITZIA")
+//                        .font(.title2)
+//                        .bold()
+//                }
                 
                 // MARK: Shopping Bag Icon
                 ToolbarItem {
